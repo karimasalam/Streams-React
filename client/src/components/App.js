5672f95fcd03b687468063fdc2f68833dd0247cd
Karim Ahmed <engineer.karim.ahmed@gmail.com>
2019-12-31 08:55:44 +0200

    all done

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 
 import StreamShow from "./streams/StreamShow";
 import StreamCreate from "./streams/StreamCreate";
@@ -13,15 +13,17 @@ import history from '../history';
 const App = () => {
   return (
     <div className="ui container">
-      
       <Router history={history}>
         <div>
           <Header />
-          <Route path="/" exact component={StreamList} />
-          <Route path="/stream/new" exact component={StreamCreate} />
-          <Route path="/stream/edit/:id" exact component={StreamEdit} />
-          <Route path="/stream/delete/:id" exact component={StreamDelete} />
-          <Route path="/stream/show" exact component={StreamShow} />
+          {/* //Switch choose first matching route only */}
+          <Switch> 
+            <Route path="/" exact component={StreamList} />
+            <Route path="/stream/new" exact component={StreamCreate} />
+            <Route path="/stream/edit/:id" exact component={StreamEdit} />
+            <Route path="/stream/delete/:id" exact component={StreamDelete} />
+            <Route path="/stream/:id" exact component={StreamShow} />
+          </Switch>
         </div>
       </Router>
     </div>
